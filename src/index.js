e32beef31abdfcc9583089d7f2a1e56bf7ba00a8
bulbyash <bulbyash@gmail.com>
2019-03-23 12:01:07 +0300

    feat: check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var arr = [];
+  var st = [];
+  var result;
+
+  //feed array
+  for (var i = 0; i < bracketsConfig.length; i++) {
+      for (var j = 0; j < 2; j++)
+    arr.push(bracketsConfig[i][j]);
+  }
+
+  for (var i = 0; i < str.length; i++) {
+      st.push(str[i]);
+  }
+
+  for (var i = 0; i < st.length; i++) {
+      for (var k = 0; k < arr.length; k = k + 2) {
+          if (st[i] == arr[k] && st[i+1] == arr[k+1]) {
+              st.splice(i, 2);
+              i = 0;
+              k=-2;
+          }
+      }
+  }
+
+  if (st.length ==0) {
+      result = true
+  } else {
+    result = false;
+  }
+
+  return result;
 }
